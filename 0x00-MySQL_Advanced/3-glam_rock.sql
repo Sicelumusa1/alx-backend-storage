1a928196db0e0563791d6ae1f99dcaf39741a86b
Sicelumusa <musaqwabe@gmail.com>
2024-03-22 16:40:47 +0000

    corrected the scripts

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,15 +3,12 @@
 SELECT
     band_name,
     CASE
-	WHEN split IS NULL THEN (2022 - YEAR(formed))
-	ELSE (YEAR(split) - YEAR(formed))
+	WHEN split IS NULL THEN (2022 - formed)
+	ELSE (split - formed)
     END AS lifespan
 FROM
     metal_bands
 WHERE
     style LIKE '%Glam rock%'
 ORDER BY
-    CASE
-        WHEN split IS NULL THEN (2022 - YEAR(formed))
-        ELSE (YEAR(split) - YEAR(formed))
-    END DESC;
+    lifespan DESC;
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -12,9 +12,12 @@ BEGIN
     -- Get the item ID and order quantity from inserted order
     SELECT item_id, number INTO item_id, order_quantity FROM items WHERE name = NEW.item_name;
 
-    -- Update the item quantity
+    -- Update the item quantity only if it remains positive
     UPDATE items
-    SET quantity = quantity - order_quantity
+    SET quantity = CASE
+        WHEN (quantity - order_quantity) >= 0 THEN quantity - order_quantity
+	ELSE quantity
+    END
     WHERE id = item_id;
 END;
 //
